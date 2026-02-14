0b7767b7f13ce50cbeb0fc0b9a6a42566f76797a
Nick Heggø <git@nheggoe.dev>
2026-02-14 22:36:30 +0100

    debug: add root endpoint to display HTTP method and path

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,11 +5,15 @@ import (
 	"countryinfo/internal/handler/exchange"
 	"countryinfo/internal/handler/info"
 	"countryinfo/internal/handler/status"
+	"fmt"
 	"net/http"
 )
 
 func New(cfg *config.Config) http.Handler {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Method: %s\nPath: %s\n", r.Method, r.URL.Path)
+	})
 	mux.HandleFunc("GET /countryinfo/v1/status", status.Handler(cfg))
 	mux.HandleFunc("GET /countryinfo/v1/info/{country_code}", info.Handler(cfg.Countries))
 	mux.HandleFunc("GET /countryinfo/v1/exchange/{country_code}", exchange.Handler(cfg.Currency))
